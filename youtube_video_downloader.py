e4f998743bccbceaa348f3474fa25ee7022bd711
Tushaar2208 <tushaarkapil22@gmail.com>
2025-03-15 15:17:18 +0530

    Update youtube_video_downloader.py

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -2,20 +2,25 @@ import yt_dlp
 import os
 
 def download_video(url):
-    desktop_path = os.path.expanduser("~/Desktop") 
+    desktop_path = os.path.expanduser("~/Desktop")  
     ydl_opts = {
         'verbose': True, 
-        'outtmpl': os.path.join(desktop_path, '%(title)s.%(ext)s')
+        'format': 'bestvideo[height<=1080]+bestaudio/best',  
+        'merge_output_format': 'mp4', 
+        'outtmpl': os.path.join(desktop_path, '%(title)s.%(ext)s'),
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',
+        }]
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
             ydl.download([url])
-            print("Video downloaded to desktop!")
+            print("Video downloaded in 1080p to desktop!")
         except Exception as e:
             print(f"An error occurred: {e}")
 
 
 url = input("Paste YouTube video URL here: ")
-
 download_video(url)
